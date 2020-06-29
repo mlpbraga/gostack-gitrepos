aa93cd11fdbbafecdb14e0a8aeb76a001a32138c
Luísa Braga <mlpb@icomp.ufam.edu.br>
2020-06-29 16:26:48 -0400

    feat: styled components

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import { Title } from './styles';
+
 const Dashboard: React.FC = () => {
-  return (<div>
-    <h1>Dashboard</h1>
-  </div>)
-}
+  return (
+    <div>
+      <Title>Explore repositórios no GitHub</Title>
+    </div>
+  );
+};
 
 export default Dashboard;
--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const Repository: React.FC = () => {
-  return (<div>
-    <h1>Repository</h1>
-  </div>)
-}
+  return (
+    <div>
+      <h1>Repository</h1>
+    </div>
+  );
+};
 
 export default Repository;
