ccd70be89cb27f89f2a9a32e76186490664dc6a5
Luísa Braga <mlpb@icomp.ufam.edu.br>
2020-06-29 20:47:50 -0400

    feat: dashboard screen

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,54 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
 
-import { Title } from './styles';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
   return (
-    <div>
+    <>
+      <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositórios no GitHub</Title>
-    </div>
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/28370139?s=460&u=65ae03943a50815a79c6a2b86f7fa82745f7d5ec&v=4"
+            alt="Girhub Image"
+          />
+          <div>
+            <strong>Titulo aqui</strong>
+            <p>descriçãoa qui</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/28370139?s=460&u=65ae03943a50815a79c6a2b86f7fa82745f7d5ec&v=4"
+            alt="Girhub Image"
+          />
+          <div>
+            <strong>Titulo aqui</strong>
+            <p>descriçãoa qui</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/28370139?s=460&u=65ae03943a50815a79c6a2b86f7fa82745f7d5ec&v=4"
+            alt="Girhub Image"
+          />
+          <div>
+            <strong>Titulo aqui</strong>
+            <p>descriçãoa qui</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
   );
 };
 
